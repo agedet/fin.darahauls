bd4c0df73b0568dc424e6550a7ac1aa6a4ca53c5
Aniekan George Edet <aniekan.g.edet@gmail.com>
2025-05-20 11:40:32 +0100

    fix: admin routes

--- src/app/dashboard/rider/layout.tsx
+++ src/app/dashboard/rider/layout.tsx
@@ -16,11 +16,12 @@ export default function RootLayout({
     <main className="flex h-screen w-full">
       <div className="hidden relative lg:flex items-center justify-center lg:w-1/2 bg-[url('/ride.jpg')] bg-cover bg-center bg-no-repeat">
         <div className="absolute inset-0 z-40 flex flex-col gap-2 h-full w-full items-center justify-center bg-gradient-to-br from-[#000000] to-[#000000]/50">
-          
+         
         </div>
       </div>
 
       <div className="w-full flex flex-col justify-center items-center px-[20px] lg:w-1/2">
+       
         {children}
         <Toaster richColors position="top-left" />
       </div>
